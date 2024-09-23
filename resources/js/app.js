06e9e908e45030ad9a0e71d3082cb3190b0b4b2a
Wilson <wtovaragudelo@gmail.com>
2024-09-23 19:43:05 +0200

    feat: topbar & footer

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,25 @@
+import 'iconify-icon'
+
 console.log('Hello World')
+
+window.onload = () => {
+  const form = document.querySelector('form')
+  const imageInput = document.querySelector('#image')
+  const selectImageBtn = document.querySelector('#upload')
+
+  selectImageBtn.onclick = () => {
+    imageInput.click()
+  }
+
+  imageInput.onchange = (ev) => {
+    const files = ev.target.files
+    const file = files[0]
+
+    if (file) {
+      selectImageBtn.setAttribute('disabled', true)
+      selectImageBtn.classList.add('loading')
+
+      form.submit()
+    }
+  }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./resources/**/*.edge', './resources/**/*.{js,ts,jsx,tsx,vue}'],
+
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ['Libre Baskerville', ...defaultTheme.fontFamily.serif],
+        sans: ['Instrument Sans', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
+
   plugins: [require('daisyui')],
+
   daisyui: {
     themes: ['cupcake'],
   },
