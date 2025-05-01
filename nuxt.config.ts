31f543ad4a42ab660317e85fa4648b403161a818
Wilson Tovar <wtovaragudelo@gmail.com>
2025-05-01 16:13:40 +0200

    chore: remove @nuxt/content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
 
-  modules: [
-    "@nuxt/content",
-    "@nuxt/eslint",
-    "@nuxt/scripts",
-    "@nuxt/ui-pro",
-    "@nuxt/image",
-  ],
+  modules: ["@nuxt/eslint", "@nuxt/scripts", "@nuxt/ui-pro", "@nuxt/image"],
 
   nitro: {
     storage: {
