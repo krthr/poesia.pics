cf02823a74855dc6f0b0ffbfbca1f2b567de77b4
Wilson Tovar <wtovaragudelo@gmail.com>
2025-05-03 19:12:48 +0200

    fix title & move og image to app.vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,7 @@ export default defineNuxtConfig({
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
       htmlAttrs: { lang: "es" },
+      title: "Poesia.pics - Convierte tus imágenes en poemas con IA",
     },
   },
 
